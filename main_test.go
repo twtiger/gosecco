ed2fd3b974d30ff43da0d23324b325165fb49ca3
Chelsea Komlo <ckomlo@thoughtworks.com>
2016-04-05 18:17:14 -0500

    YD/CK Adding test infrastructure

---
+++ main_test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"testing"
+  . "gopkg.in/check.v1"
+)
+
+// Hook up gocheck into the "go test" runner.
+func Test(t *testing.T) { TestingT(t) }
+
+type MySuite struct{}
+
+var _ = Suite(&MySuite{})
+
+func (s *MySuite) TestHelloWorld(c *C) {
+    c.Assert(42, Equals, TestGo())
+}
