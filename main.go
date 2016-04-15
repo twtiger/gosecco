cefff0b091824519195e84737bf51ac5bb38cdd7
Ola Bini <ola@olabini.se>
2016-04-15 17:24:18 -0500

    Stupid reference in main file, not necessary

--- main.go
+++ main.go
@@ -2,20 +2,19 @@ package main
 
 import (
 	"fmt"
-
-	seccomp "github.com/subgraph/go-seccomp"
+	"go/parser"
 )
 
 func main() {
-	result, _ := seccomp.Compile("bla.seccomp", true)
-	for _, filter := range result {
-		fmt.Println(filter)
-	}
-
-	// result, err := parser.ParseFile("profiles/shared.seccomp")
-	// if err != nil {
-	// 	fmt.Printf("Error: %s\n", err)
-	// } else {
-	// 	fmt.Printf("Result: %#v\n", result)
+	// result, _ := seccomp.Compile("bla.seccomp", true)
+	// for _, filter := range result {
+	// 	fmt.Println(filter)
 	// }
+
+	result, err := parser.ParseFile("profiles/shared.seccomp")
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+	} else {
+		fmt.Printf("Result: %#v\n", result)
+	}
 }
