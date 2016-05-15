70670aefc9638c75612d31cec33d9ce25cc48ae4
Chelsea Komlo <chelsea.komlo@gmail.com>
2016-05-15 13:55:08 -0500

    test that parse errors are returned from API interface

--- seccomp_test.go
+++ seccomp_test.go
@@ -1,6 +1,9 @@
 package gosecco
 
 import (
+	"os"
+	"path"
+	"strings"
 	"testing"
 
 	"golang.org/x/sys/unix"
@@ -18,4 +21,20 @@ func (s *SeccompSuite) Test_loadingTooBigBpf(c *C) {
 	inp := make([]unix.SockFilter, 0xFFFF+1)
 	res := Load(inp)
 	c.Assert(res, ErrorMatches, "filter program too big: 65536 bpf instructions \\(limit = 65535\\)")
+}
+
+func getActualTestFolder() string {
+	wd, _ := os.Getwd()
+	if strings.HasSuffix(wd, "/parser") {
+		return wd
+	}
+	return path.Join(wd, "parser")
+}
+
+func (s *SeccompSuite) Test_parseInvalidFileReturnsErrors(c *C) {
+	set := SeccompSettings{}
+	f := getActualTestFolder() + "/failing_test_policy"
+	_, ee := Prepare(f, set)
+	c.Assert(ee, ErrorMatches, ".*parser/failing_test_policy:1: unexpected end of line")
+
 }
